9f7cff944db2d9fc3aa938abc03f45bd97c8d474
Peter Morris <mrpmorris@gmail.com>
2022-12-23 14:22:57 +0000

    IDbSet

---
+++ Source/Lib/Morris.DomainDrivenDesign.MongoDb/DbSet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Morris.DomainDrivenDesign.MongoDb;
+
+internal interface IDbSet<TKey>
+	where TKey: IEquatable<TKey>
+{
+	Task SaveCollectionChangesAsync(IEnumerable<EntityEntry<TKey>> entityEntries);
+}
